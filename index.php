6be9e05a3813e1a112bf6ca63f569fb059174c5a
jari <jaristephan@outlook.com>
2018-01-11 12:51:10 +0100

    basic layout done for now

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
 <?php
-  // require_once("system/superPlugin.php");
-  // $plugin = require_once("plugins/page/plugin.php");
-  // $tagEngine = require_once("system/tagEngine.php");
-  // $plugin->registerTags($tagEngine);
-  // echo $tagEngine->getTagMethod("page")();
-  // require_once("system/superPlugin.php");
-  // require_once("plugins/page/plugin.php");
-  // require_once("system/tagEngine.php");
-  // require_once("system/templateEngine.php");
   require_once("system/classLoader.php");
 
+  //
   $tagEngine = new TagEngine();
   $templateEngine = new TemplateEngine($tagEngine);
 
   //load all plugins
-  $pluginLoader = new pluginLoader();
-  $pluginLoader->load($tagEngine);
+  $pluginManager = new pluginManager();
+  $pluginManager->load($tagEngine);
 
   $page = file_get_contents("layout/index.html");
   $output = $templateEngine->build($page);
--- system/pluginManager.php
+++ system/pluginManager.php
@@ -1,5 +1,5 @@
 <?php
-  class pluginLoader {
+  class pluginManager {
 
     private $plugins = array();
 
