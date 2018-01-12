1614e88e028652623b007529216870e980cf0d23
jari <jaristephan@outlook.com>
2018-01-12 19:15:34 +0100

    more rogue files!

--- panel/plugins.php
+++
@@ -1,8 +0,0 @@
-<?php
-  if(isset($_GET['plugin'])) {
-    $urlKey = $_GET['plugin'];
-    if(file_exists("plugins/$urlKey/panel.php")) {
-      require_once("plugins/$urlKey/panel.php");
-    }
-  }
-?>
