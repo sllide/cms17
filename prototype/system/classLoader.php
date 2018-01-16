3190c024dbf1f58fc42dd42c49716e2a2e9c6e62
Jari Stephan <jaristephan@outlook.com>
2018-01-16 15:29:54 +0100

    removed prototype

--- prototype/system/classLoader.php
+++
@@ -1,9 +0,0 @@
-<?php
-  function systemLoader($class) {
-      if(file_exists('../system/' . $class . '.php')) include '../system/' . $class . '.php';
-      if(file_exists('../system/database/' . $class . '.php'))include '../system/database/' . $class . '.php';
-      if(file_exists('../system/admin/' . $class . '.php'))include '../system/admin/' . $class . '.php';
-  }
-
-  spl_autoload_register('systemLoader');
-?>
