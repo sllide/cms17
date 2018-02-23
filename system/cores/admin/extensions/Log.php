881f168af7bcc239c58a715783e380c771043e31
Jari <jaristephan@outlook.com>
2018-02-23 15:04:24 +0100

    stuffs

--- system/cores/admin/extensions/Log.php
+++ system/cores/admin/extensions/Log.php
@@ -2,11 +2,17 @@
   return new class {
     function build() {
       Template::addTag('list', [$this, 'logList']);
-      return '@@list@@';
+      return File::getTemplate('log/index');
     }
 
     function logList() {
-    
+      $data = "";
+      $log = Database::getAllFromTable('log');
+      foreach($log as $row) {
+        $entry = "<tr><td>%s</td><td>%s</td><td>%s</td></tr>";
+        $data .= sprintf($entry, $row['invoker'], $row['type'], $row['message']);
+      } 
+      return $data;
     }
   }
 ?>
